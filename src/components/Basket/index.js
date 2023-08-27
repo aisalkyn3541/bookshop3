5ce08af893979947b369acf47c9015ff6e710dc6
Asad <asadkhakimov05@gmail.com>
2023-08-27 22:15:59 +0600

    add

---
+++ src/components/Basket/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import BasketCard from "./BasketCart";
+const Basket = () => {
+  window.scrollTo(0, 0);
+  const { basket } = useSelector((s) => s);
+  return (
+    <div id="cart">
+      <div className="container">
+        <div class="cart__text">
+          <h1>Your cart</h1>
+          <p>
+            Not ready to checkout? <span>Continue Shopping</span>{" "}
+          </p>
+        </div>
+        {basket.length === 0 ? (
+          <h1 style={{ color: "red", paddingTop: "50px" }}>
+            Ваша карзина пусто!!!
+          </h1>
+        ) : (
+          basket.map((el) => <BasketCard el={el} key={el.id} />)
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Basket;
