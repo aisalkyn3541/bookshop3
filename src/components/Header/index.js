ba2b7b3a73899259c7d6a5263edd0543c2fee91b
bilalbek2811 <142898214+bilalbek2811@users.noreply.github.com>
2023-09-02 17:27:19 +0600

    uefa (#18)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,28 +5,50 @@ import { CiSearch } from "react-icons/ci";
 import { AiOutlineShopping } from "react-icons/ai";
 import { Link, NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 const Header = () => {
   const { basket } = useSelector((r) => r);
+  const [search, setSearch] = useState(true);
+
   return (
     <div id="header">
       <div className="container">
         <div className="header">
           <div className="header--nav">
-            <NavLink to={"/"}>
+            <NavLink to={"/"} onClick={() => window.scroll(0, 0)}>
               <img src={logo} alt="" />
             </NavLink>
-            <NavLink to={"/categories"}>Categories</NavLink>
+            <NavLink to={"/"} onClick={() => window.scroll(0, 800)}>
+              Categories
+            </NavLink>
             <NavLink to={"/newbooks"}>Recent</NavLink>
             <NavLink to={"/books"}>Books</NavLink>
             <NavLink to={"/aboutus"}>About Us</NavLink>
           </div>
           <div className="header--icons">
-            <CiSearch style={{fontSize: '20px', marginRight: '10px'}}/>
+            <div
+              className="header--icons__logo"
+              style={{
+                background: search ? "transparent" : "",
+                marginTop: "-4",
+              }}
+            >
+              <CiSearch
+                className="header--icons__logo--search"
+                onClick={() => {
+                  setSearch(!search);
+                }}
+                style={{ fontSize: "20px", marginRight: "10px" }}
+              />
+              <input type="text" style={{ width: search ? "" : "120px" }} />
+            </div>
             <Link to={"/basket"}>
-              <AiOutlineShopping style={{
-                fontSize: '20px',
-                color: '#000'
-              }} />
+              <AiOutlineShopping
+                style={{
+                  fontSize: "20px",
+                  color: "#000",
+                }}
+              />
             </Link>
             <sup>{basket.length}</sup>
           </div>
